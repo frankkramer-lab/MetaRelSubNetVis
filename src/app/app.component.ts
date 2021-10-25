e94d74740e697b7d635fd671b99ee6522be98d08
Simone Mayer <simone1.mayer@student.uni-augsburg.de>
2021-10-25 16:16:10 +0200

    [WIP] sidebar collapsible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import * as cytoscape from 'cytoscape';
+// @ts-ignore
+import * as svg from 'cytoscape-svg';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'meta';
+export class AppComponent implements AfterViewInit {
+  /**
+   * Icon: faBars
+   */
+  faBars = faBars;
+
+  /**
+   * Icon: faTimes
+   */
+  faTimes = faTimes;
+
+  /**
+   * True, if the sidebar is visible
+   */
+  showSidebar = true;
+
+  /**
+   * Cytoscape core which contains the essential functionality
+   */
+  cy!: cytoscape.Core;
+
+  /**
+   * Cytoscape container used for rendering the network
+   */
+  @ViewChild('cy') cyContainer!: ElementRef;
+
+  ngAfterViewInit(): void {
+    cytoscape.use(svg);
+    this.cy = cytoscape({
+      container: this.cyContainer.nativeElement,
+      elements: [
+        {
+          group: 'nodes',
+          data: {
+            id: 'n1',
+            parent: 'nparent',
+          },
+
+          scratch: {
+            _foo: 'bar',
+          },
+
+          position: {
+            x: 100,
+            y: 100,
+          },
+        },
+      ],
+
+      layout: {
+        name: 'preset',
+      },
+
+      // so we can see the ids
+      style: [
+        {
+          selector: 'node',
+          style: {
+            label: 'data(id)',
+          },
+        },
+      ],
+    });
+  }
+
+  exportSvg(): void {
+    // @ts-ignore
+    console.log(this.cy.svg());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
